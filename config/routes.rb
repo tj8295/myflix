430e0c49fc13cdc4797e0c72367a193f827f2ae9
tj8295 <tch399@gmail.com>
2014-02-03 16:37:06 -0500

    added testing for validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
+
+  root to: 'videos#index'
+  get '/home', to: 'videos#index'
+
+  resources :videos
+  resources :categories
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# Create videos
+Video.create(title: "Monk", description: "Detective with a sixth sense", small_cover_url: "monk.jpg", large_cover_url: "monk_large.jpg")
+Video.create(title: "Family Guy", description: "Family with a smart baby", small_cover_url: "family_guy.jpg")
+Video.create(title: "South Park", description: "Kids from Colorado", small_cover_url: "south_park.jpg")
+Video.create(title: "Futurama", description: "A boy and his robot", small_cover_url: "futurama.jpg")
+
+# Create Categories
+Category.create(name: "TV Commedies")
+Category.create(name: "TV Dramas")
