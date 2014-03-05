93ca38c62c320298845eb25a321c828e4a0caad4
tj8295 <tch399@gmail.com>
2014-03-05 15:50:51 -0500

    took method out of includes

--- lib/tokenable.rb
+++ lib/tokenable.rb
@@ -3,8 +3,10 @@ module Tokenable extend ActiveSupport::Concern
   included do
     before_create :generate_token
 
+
+  end
+
     def generate_token
       self.token = SecureRandom.urlsafe_base64
     end
-  end
 end
