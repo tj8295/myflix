f673b891b6386468cb7c0411f969492710b388d4
tj8295 <tch399@gmail.com>
2014-03-14 12:11:51 -0400

    handles decoration with draper

---
+++ app/services/user_registration.rb
@@ -0,0 +1,22 @@
+class UserRegistion
+  attr_accessor :user
+
+  def initialize(user)
+    @user = user
+  end
+
+  def pay
+
+  end
+
+
+  def invite
+
+  end
+
+  def notify_by_email
+
+  end
+end
+
+#move a lot of the controller test to a test of this class. Be sure to use stub/mocks
